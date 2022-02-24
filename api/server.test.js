772d364438623c3ca6f3c505db4b4efc42d6caa9
Amethyst Willia <zmithic@gmail.com>
2022-02-24 15:52:04 -0600

    Tested to check if table starts empty, passed

--- api/server.test.js
+++ api/server.test.js
@@ -17,6 +17,12 @@ test('verify we are using the correct environment', ()  => {
     expect(process.env.NODE_ENV).toBe('testing');
 });
 
-describe('Test cats model', () => {});
+describe('Test cats model', () => {
+    test('Table starts empty', async () => {
+        const kitties = await db('cats');
+
+        expect(kitties).toHaveLength(0);
+    });
+});
 
 describe('Test server endpoints', () => {});
