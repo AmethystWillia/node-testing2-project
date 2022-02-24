91862315e4d7f8a708947ad1774e63a9d5e458e5
Amethyst Willia <zmithic@gmail.com>
2022-02-24 16:33:03 -0600

    Addec code to get/:id endpoint, passes

--- api/server.js
+++ api/server.js
@@ -15,7 +15,11 @@ server.get('/cats', (req, res, next) => {
 });
 
 server.get('/cats/:id', (req, res, next) => {
-    res.json();
+    Cats.getById(req.params.id)
+        .then(cat => {
+            res.status(200).json(cat);
+        })
+        .catch(next);
 });
 
 server.post('/cats', (req, res, next) => {
